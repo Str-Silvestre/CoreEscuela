d93e884615d04694279a1c5b7d73a6f2a726618d
Kelvin Silvestre <StrSilvestre619@users.noreply.github.com>
2020-06-30 17:47:03 -0400

    Proyecto escuelaCore etapa1

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using CoreEscuela.Entidades;
+using static System.Console;
+
+namespace Etapa1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var escuela = new Escuela("Cenforcasa", 2015, TiposEscuela.Primaria);
+            escuela.Pais = "Rep. Dom.";
+            escuela.Ciudad = "San Cristobal";
+
+            escuela.TipoEscuela = TiposEscuela.Primaria;
+            escuela.Cursos = new Curso[] {
+                new Curso{Nombre = "101"},
+                new Curso{Nombre = "201"},
+                new Curso{Nombre = "301"}
+            };
+
+
+            ImprimirCursosEscuela(escuela);
+
+        }
+
+        private static void ImprimirCursosEscuela(Escuela escuela)
+        {
+            WriteLine("=======================");
+            WriteLine("Cursos De la Escuela");
+            WriteLine("=======================");
+            if (escuela?.Cursos != null)
+            {
+                foreach (var curso in escuela.Cursos)
+                {
+                    WriteLine($" Nombre: {curso.Nombre}, Id: {curso.UniqueId}");
+                }
+            }
+
+        }
+
+        private static void ImprimirCusos(Curso[] listCursos)
+        {
+            int contador = 0;
+            while (contador < listCursos.LongLength)
+            {
+                Console.WriteLine($"Nombre: {listCursos[contador].Nombre}, Id: {listCursos[contador].UniqueId} ");
+
+                contador++;
+            }
+        }
+    }
+}
