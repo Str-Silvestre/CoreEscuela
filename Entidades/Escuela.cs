a6a92a20e4ccc550432c5ec7c5542e7bc01d5611
Str Silvestre <skell619@gmail.com>
2020-07-02 18:58:05 -0400

    Refactorice el codigo para organizar

--- Entidades/Escuela.cs
+++ Entidades/Escuela.cs
@@ -3,7 +3,7 @@ using System;
 namespace CoreEscuela.Entidades
 {
     
-    class Escuela
+     public class Escuela
     {
         string nombre;
 
--- Program.cs
+++ Program.cs
@@ -1,78 +1,42 @@
-﻿using System.Runtime.ConstrainedExecution;
-using System.Collections.Generic;
-using System.Drawing;
-using System;
+﻿using System;
 using CoreEscuela.Entidades;
+using CoreEscuela.Util;
 using static System.Console;
 
-namespace Etapa1
+namespace CoreEscuela
 {
     class Program
     {
         static void Main(string[] args)
         {
 
-            Console.BackgroundColor = ConsoleColor.DarkGray;
-            var escuela = new Escuela("Cenforcasa", 2015, TiposEscuela.Primaria);
-            escuela.Pais = "Rep. Dom.";
-            escuela.Ciudad = "San Cristobal";
+            //Console.BackgroundColor = ConsoleColor.DarkGray;
+            
+            var engine = new EscuelaEngine();
 
-            escuela.TipoEscuela = TiposEscuela.Primaria;
+            engine.Inicializar(); 
 
-            escuela.Cursos = new List<Curso>(){
-                new Curso{Nombre = "101", Jornada = TiposJornada.Mañana},
-                new Curso{Nombre = "201", Jornada = TiposJornada.Mañana},
-                new Curso{Nombre = "301", Jornada = TiposJornada.Mañana}
-            };
-
-            escuela.Cursos.Add( new Curso{ Nombre = "102", Jornada = TiposJornada.Tarde});
-            escuela.Cursos.Add( new Curso{ Nombre = "202", Jornada = TiposJornada.Tarde});
-
-            var otrosCursos = new List<Curso>(){
-                new Curso{Nombre = "401", Jornada = TiposJornada.Mañana},
-                new Curso{Nombre = "501", Jornada = TiposJornada.Mañana},
-                new Curso{Nombre = "501", Jornada = TiposJornada.Noche}
-            };
-
-            escuela.Cursos.AddRange(otrosCursos);
-            escuela.Cursos.RemoveAll(delegate(Curso cur)
-                                       {    
-                                           return cur.Nombre == "301";
-
-                                       });
-
-            escuela.Cursos.RemoveAll((cur)=> cur.Nombre == "501" && cur.Jornada == TiposJornada.Noche);
-            ImprimirCursosEscuela(escuela);
-
-
+            Printer.WriteTitulo("BIENVENIDOS A LA ESCUELA");        
+                        
+            ImprimirCursosEscuela(engine.Escuela);
 
         }
         
         private static void ImprimirCursosEscuela(Escuela escuela)
         {
-            WriteLine("=======================");
-            WriteLine("Cursos De la Escuela");
-            WriteLine("=======================");
+            Printer.WriteTitulo("Cursos de la Escuela");
+            
             if (escuela?.Cursos != null)
             {
                 foreach (var curso in escuela.Cursos)
                 {
                     WriteLine($" Nombre: {curso.Nombre}, Id: {curso.UniqueId}");
+                    Printer.Pito();
                 }
             }
 
         }
 
-        private static void ImprimirCusos(Curso[] listCursos)
-        {
-            int contador = 0;
-            while (contador < listCursos.LongLength)
-            {
-
-                WriteLine($"Nombre: {listCursos[contador].Nombre}, Id: {listCursos[contador].UniqueId} ");
-
-                contador++;
-            }
-        }
+       
     }
 }
